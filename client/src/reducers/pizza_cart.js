eefb1a246debc2e3866aaf487999ee099238308a
Kastriot78 <67861812+Kastriot78@users.noreply.github.com>
2020-08-28 13:05:10 +0200

    Update pizza_cart.js

--- client/src/reducers/pizza_cart.js
+++ client/src/reducers/pizza_cart.js
@@ -7,6 +7,8 @@ export default function(state = initialState , action) {
             return {items: action.payload.cartItems}
         case 'QUANTITY_MINUS':
             return {items: action.payload.cartItemss}
+        case 'QUANTITY_PLUS':
+            return {items: action.payload.cartItemsss}
         case 'REMOVE_FROM_CART':
             return {items: action.payload.cartRemoveItems}
         default:
