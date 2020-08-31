f062c76f87046f5d746c4210717317e41d27b473
Kastriot78 <67861812+Kastriot78@users.noreply.github.com>
2020-09-01 00:45:20 +0200

    Update pizza.js

--- client/src/components/Pizza/pizza.js
+++ client/src/components/Pizza/pizza.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { getAllPizzas, addToCart } from '../../actions/index';
 
 import './pizza.css';
 
 class Pizza extends Component {
-    
+
+    state = {
+        search: ''
+    }
+
+    updateSearch = (event) => {
+        this.setState({search: event.target.value})
+    }
+
     componentWillMount() {
         this.props.dispatch(getAllPizzas());
     }
 
     addPizzaToCart = (product) => {
         this.props.dispatch(addToCart(this.props.pizzaCart.items,product))
+        let data = this.props.pizzaCart.items.find(item => item[0] == product[0]) //sepse product[0] osht id - ne php kshtu vjen objekti prandaj
+    
+        if(data) {
+         toast.error('Product exists on cart!');
+        } else {
+          toast.success('Product added to cart.');
+        }
     }
     
     renderProducts = (products) => {
+        let filteredProducts = products.allPizzas ? products.allPizzas.filter((pizza) => {
+            return pizza[1].indexOf(this.state.search) !== -1; // [0] id,[1] is name -> from php
+        }) : null;
         return (
-            products.allPizzas ?
-            products.allPizzas.map(item => {
+            filteredProducts ?
+            filteredProducts.map(item => {
                 return (
-                    <div className="col-md-3 col-sm-6" key={item.id}>
+                    <div className="col-md-3 col-sm-6 mt-3" key={item.id}>
                             <div className="product-grid2">
                                 <div className="product-image2">
                                     <a href="javascript:;;;">
@@ -38,7 +58,6 @@ class Pizza extends Component {
                                     <span class="price">$ {item[4]}</span>
                                 </div>
                             </div>
-                            {/* {this.state.inCart?<span>ADDED to cart</span> : ''} */}
                         </div>
                         
                 )
@@ -50,9 +69,22 @@ class Pizza extends Component {
     render() {
         return (
             <div id="pizza">
+            <div className="container d-flex justify-content-center">
+              <div className="active-cyan-4 col-md-8 col-sm-6 mt-3">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={this.state.search}
+                  onChange={this.updateSearch}
+                />
+              </div>
+            </div>
                 <div className="container">
                     <div className="row">
                         {this.renderProducts(this.props.pizzas)}
+                        <ToastContainer />
                     </div>
                 </div>
             </div>
